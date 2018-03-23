e2c0ee80cd679e371f9591de1dd23c1809e5bacd
jczetta <jczetta@gmail.com>
2018-03-22 23:07:41 -0400

    Add all login routes

--- SI364_twitterapp.py
+++ SI364_twitterapp.py
@@ -79,7 +79,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(255), unique=True, index=True)
     email = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
-    tweets = db.relationship('tweets',backref='User')
+    tweets = db.relationship('Tweet',backref='User')
 
     @property
     def password(self):
@@ -182,6 +182,43 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
+# Login-related routes
+@app.route('/login',methods=["GET","POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('index'))
+        flash('Invalid username or password.')
+    return render_template('login.html',form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out')
+    return redirect(url_for('index'))
+
+@app.route('/register',methods=["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,username=form.username.data,password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You can now log in!')
+        return redirect(url_for('login'))
+    return render_template('register.html',form=form)
+
+@app.route('/secret')
+@login_required
+def secret():
+    return "Only authenticated users can do this! Try to log in or contact the site admin."
+
 
 ## Main route
 
